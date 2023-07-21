a418423bf9d796c31f3ddd2f03d480c60848c174
Ahsan4k <83755681+Ahsan4k@users.noreply.github.com>
2023-07-21 07:22:21 +0500

    Update auth.ts

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,4 @@
-var express = require("express")
-const router = express.Router();
+const router = require("express").Router();
 
 const {Register, Login, Forgot, verifyNumber, Logout} = require('../controllers/authControllers')
 
